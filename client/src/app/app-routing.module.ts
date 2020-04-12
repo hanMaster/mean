4869528c2fac44f768bab00da095eed5e95ebb5f
hanMaster <w54661c@gmail.com>
2020-04-12 14:41:17 +1000

    Site authorization finished

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { AuthGuard } from './shared/classes/auth.guard';
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent },
+    ],
+  },
+  { path: '', component: SiteLayoutComponent, canActivate:[AuthGuard] ,children: [] },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { MaterialService } from '../shared/classes/material.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
+  form: FormGroup;
+  aSub: Subscription;
+  routeSub: Subscription;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+    this.routeSub = this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        MaterialService.toast('Теперь вы можете зайти в систему используя свои данные')
+      } else if (params['accessDenied']) {
+        MaterialService.toast('Для начала авторизуйтесь в системе')
+      }else if(params['sessionExpired']){
+        MaterialService.toast('Сессия закончилась, пожалуйста авторизуйтесь.')
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
+
+  onSubmit() {
+    this.form.disable();
+    this.aSub = this.authService.login(this.form.value).subscribe(
+      (token) =>
+        this.router.navigate(['/overview']),
+      (error) => {
+        MaterialService.toast(error.error.message)
+        this.form.enable();
+      }
+    );
+  }
 }
